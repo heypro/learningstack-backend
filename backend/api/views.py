25ddde6712a0a50141004ca7fa63df1c982ceb92
heypro <blockers.offical@gmail.com>
2025-05-19 14:42:17 +0000

    final fixes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def auth_view(request):
+    return JsonResponse({'message': 'Auth endpoint hit'})
--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # <--- this is the important line
+    path('api/', include('backend.api.urls')),  # <--- this is the important line
 ]
