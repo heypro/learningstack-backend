3184a733504b7f68062d76cbc7e3c94817b55b46
heypro <blockers.offical@gmail.com>
2025-05-19 14:23:50 +0000

    awed

--- backend/telegram_auth/apps.py
+++ backend/telegram_auth/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TelegramAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'telegram_auth'
+    name = 'backend.telegram_auth'
