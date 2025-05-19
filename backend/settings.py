021abb5ced62488798623133f85f42cc7e2fec5e
heypro <blockers.offical@gmail.com>
2025-05-19 14:14:31 +0000

    tired

--- backend/settings.py
+++ backend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'telegram_auth',
+    'backend.api',
+    'backend.telegram_auth',
 ]
 
 MIDDLEWARE = [
